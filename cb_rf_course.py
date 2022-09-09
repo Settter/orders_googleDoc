3e372a021797beee9029d1c99c467514ec4ab368
Settter <egor-udon@mail.ru>
2022-09-09 19:57:28 +0300

    update sheets method

--- cb_rf_course.py
+++ cb_rf_course.py
@@ -4,7 +4,7 @@ path = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 
 # get relevant dollar to the ruble exchange rate
 def get_rub_value(usd_value):
-    return path['Valute']['USD']['Value'] * usd_value
+    return round(path['Valute']['USD']['Value']) * usd_value
 
 
 
--- doc_save.py
+++ doc_save.py
@@ -1,26 +1,28 @@
-import csv
-import io
-import urllib.request
-from db_methods import save_values
-from db_methods import delete_values
+import gspread as gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 from cb_rf_course import get_rub_value
+from db_methods import save_values, delete_values
+
+gscope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+gcredentials = 'token.json'
+gdocument = 'Копия test'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(gcredentials, gscope)
+gc = gspread.authorize(credentials)
+wks = gc.open(gdocument).sheet1
+
 
 # get values from google sheets and save it to database
 def save_db_values():
     delete_values()
 
-    url = 'https://docs.google.com/spreadsheets/d/11FkagmZbctyjr0QQRksIBKCIlN_vxY8BhHcniBG43OA/export?format=csv'
-
-    response = urllib.request.urlopen(url)
-
-    with io.TextIOWrapper(response, encoding='utf-8') as f:
-        reader = csv.reader(f)
-
-        for row in reader:
-            if row[2] == 'стоимость,$':
-                continue
-            else:
-                x = round(get_rub_value(int(row[2])), 2)
-                save_values(row[0], row[1], row[2], row[3], x)
-        print('successfully saved values')
+    values_list1 = wks.col_values(1)
+    values_list2 = wks.col_values(2)
+    values_list3 = wks.col_values(3)
+    values_list4 = wks.col_values(4)
+    x = len(values_list1)
+    i = 1
+    while i < x:
+        save_values(values_list1[i], values_list2[i], values_list3[i], values_list4[i], round(get_rub_value(int(values_list3[i])), 2))
+        i += 1
     pass
